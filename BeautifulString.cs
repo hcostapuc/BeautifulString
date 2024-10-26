049e7e262a27338b6d6a2529df9916b9f9598afb
Hebert Costa Andrade <hcostapuc@gmail.com>
2024-10-26 07:09:42 -0300

    Add project files.

---
+++ BeautifulString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BeautifulString
+{
+    internal static class BeautifulString
+    {
+        /// <summary>
+        /// Compare if the string is beautiful by checking whether the predecessor letter has fewer occurrences than the successor letter.
+        /// </summary>
+        /// <param name="inputString">string in wich will be compared</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentException">inputString that contains char that not represent letter</exception>
+        internal static bool IsBeautifulString(this string inputString)
+        {
+            if (!Regex.IsMatch(inputString, "^[a-zA-Z]+$"))
+                throw new ArgumentException("Invalid input string");
+
+            var letterCountCollection = inputString.OrderBy(x => x)
+                                                   .GroupBy(x => x).Select(x => x.ToList().Count)
+                                                   .ToList();
+            var isBeautiful = true;
+            for (int i = 0; letterCountCollection.Count-1 > i; i++) 
+            {
+                if(letterCountCollection[i] > letterCountCollection[i + 1])
+                {
+                    isBeautiful = false;
+                    break;
+                }
+            }
+            return isBeautiful;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using BeautifulString;
+
+Console.WriteLine("_________________________");
+Console.WriteLine("Beatiful string challange");
+Console.WriteLine("_________________________");
+
+var firstTest = "aaabbbbccdddd";
+var secondTest = "aabbbcccdddd";
+
+Console.WriteLine($"aaabbbbccdddd It's beautiful? {firstTest.IsBeautifulString()}");
+Console.WriteLine($"aabbbcccdddd It's beautiful? {secondTest.IsBeautifulString()}");
+Console.ReadKey();
